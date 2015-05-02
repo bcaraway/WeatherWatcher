b86353e9cb2ed885e6973d54238d29ce9c2b1d3c
Blake Caraway <bcaraway@server.fake>
2015-05-02 16:43:39 -0500

    initial commit...with beer

---
+++ WeatherWatcher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ForecastIO;
+
+namespace WeatherWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello there");
+            
+            var request = new ForecastIORequest("b7c1ba3c892accf81964d6984d626646", 30.079874f, -99.169474f, Unit.si);
+            var response = request.Get();
+            Console.ReadLine();
+        }
+    }
+}
